9c8532d21089647a4a4dc99f5ad239bad17b6190
Seungwoo Lee <outtoin@kaist.ac.kr>
2017-06-26 20:17:19 +0900

    lab13

--- lab11-xx.py
+++ lab11-xx.py
@@ -7,6 +7,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 learning_rate = 0.001
 training_epochs = 15
 batch_size = 100
+keep_prob = tf.placeholder(tf.float32)
 
 X = tf.placeholder(tf.float32, [None, 784])
 X_img = tf.reshape(X, [-1, 28, 28, 1])
@@ -19,6 +20,7 @@ L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
 L1 = tf.nn.relu(L1)
 L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[
                     1, 2, 2, 1], padding='SAME')
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
 # L2 ImgIn
 W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
@@ -27,14 +29,30 @@ L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
 L2 = tf.nn.relu(L2)
 L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[
                     1, 2, 2, 1], padding='SAME')
-L2 = tf.reshape(L2, [-1, 7 * 7 * 64])
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 
-W3 = tf.get_variable("W3", shape=[7 * 7 * 64, 10],
+W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
+
+L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
+L3 = tf.nn.relu(L3)
+L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1], strides=[
+                    1, 2, 2, 1], padding='SAME')
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
+L3 = tf.reshape(L3, [-1, 4 * 4 * 128])
+
+
+W4 = tf.get_variable("W4", shape=[4 * 4 * 128, 625],
                      initializer=tf.contrib.layers.variance_scaling_initializer())
-b = tf.Variable(tf.random_normal([10]))
+b4 = tf.Variable(tf.random_normal([625]))
 
-hypothesis = tf.matmul(L2, W3) + b
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
 
+W5 = tf.get_variable("W5", shape=[625, 10],
+                     initializer=tf.contrib.layers.variance_scaling_initializer())
+b5 = tf.Variable(tf.random_normal([10]))
+
+hypothesis = tf.matmul(L4, W5) + b5
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
     logits=hypothesis, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
@@ -49,7 +67,7 @@ for epoch in range(training_epochs):
 
     for i in range(total_batch):
         xs, ys = mnist.train.next_batch(batch_size)
-        feed_dict = {X: xs, Y: ys}
+        feed_dict = {X: xs, Y: ys, keep_prob: 0.7}
         c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
         avg_cost += c / total_batch
 
@@ -60,4 +78,24 @@ print('Learning Finished!')
 correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print('Accuracy:', sess.run(accuracy, feed_dict={
-      X: mnist.test.images, Y: mnist.test.labels}))
+      X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1}))
+
+'''
+Epoch: 0001 cost = 0.420748609
+Epoch: 0002 cost = 0.096420998
+Epoch: 0003 cost = 0.071427743
+Epoch: 0004 cost = 0.058612615
+Epoch: 0005 cost = 0.051127817
+Epoch: 0006 cost = 0.045446303
+Epoch: 0007 cost = 0.040618250
+Epoch: 0008 cost = 0.036364187
+Epoch: 0009 cost = 0.035273487
+Epoch: 0010 cost = 0.034138392
+Epoch: 0011 cost = 0.031272770
+Epoch: 0012 cost = 0.029470185
+Epoch: 0013 cost = 0.027937532
+Epoch: 0014 cost = 0.025937097
+Epoch: 0015 cost = 0.024596626
+Learning Finished!
+Accuracy: 0.9937
+'''
