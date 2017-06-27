4a932b197e2f258365ae91b3091fdd9711e25828
Seungwoo Lee <outtoin@kaist.ac.kr>
2017-06-27 16:48:43 +0900

    lab 12-2

---
+++ ML_lab12-2.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy
+tf.set_random_seed(777)
+
+
+idx2char = ['h', 'i', 'e', 'l', 'o']
+
+
+x_data = [[0, 1, 0, 2, 3, 3]]
+x_one_hot = [[[1, 0, 0, 0, 0],   # h 0
+              [0, 1, 0, 0, 0],   # i 1
+              [1, 0, 0, 0, 0],   # h 0
+              [0, 0, 1, 0, 0],   # e 2
+              [0, 0, 0, 1, 0],   # l 3
+              [0, 0, 0, 1, 0]]]  # l 3
+
+y_data = [[1, 0, 2, 3, 3, 4]]
+
+num_classes = 5
+input_dim = 5
+hidden_size = 5
+batch_size = 1
+sequence_length = 6
+learning_rate = 0.1
+
+X = tf.placeholder(tf.float32, [None, sequence_length, input_dim])
+Y = tf.placeholder(tf.int32, [None, sequence_length])
